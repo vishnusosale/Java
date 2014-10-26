7197f6bab916cc77680c725e83af38e1796472e1
Vishnu Sosale <sosalevishnu@gmail.com>
2014-10-26 12:42:09 +0530

    Add arrays example 1

---
+++ Arrays/src/Example1/Main.java
@@ -0,0 +1,35 @@
+package example1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int[] arr1;
+
+
+		System.out.println("enter array size");
+		int size = sc.nextInt(); // size of the array
+
+		arr1 = new int[size];
+		for (int i = 0; i < arr1.length; i++)
+			arr1[i] = i;
+		System.out.println("printing int type array");
+		for (int i = 0; i < arr1.length; i++)
+			System.out.println(arr1[i]);
+
+		double[] arr2;
+		arr2 = new double[size];
+		double d = 123.456;
+		for (int i = 0; i < arr2.length; i++) {
+			arr2[i] = d;
+			d = d + 895.235;
+		}
+		System.out.println("printing double type array");
+		for (double d1 : arr2) // using for-each loop
+			System.out.println(d1);
+
+	}
+}
